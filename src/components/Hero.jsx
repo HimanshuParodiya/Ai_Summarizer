b804d274ff8acf6dee4c82f3ca1f8ff543069221
Himanshu <himanshuparodiya@gmail.com>
2023-09-18 18:52:46 +0530

    Done with our Home Section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="Summarizer_Logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://himanshu-me.netlify.app/")}
+          className="black_btn"
+        >
+          About Me
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Article with <br className="max-md:hidden" />{" "}
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Unlock the Power of Efficient Reading with Summarizer: Your Go-To Tool
+        for Effortlessly Condensing Long Articles into Clear and Precise
+        Summaries, Enhancing Your Comprehension and Saving You Valuable Time.
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
