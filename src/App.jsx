d0175e15aa97fecc5a5abe19c64f167ead5b1ce9
Himanshu <himanshuparodiya@gmail.com>
2023-09-18 18:25:43 +0530

    Structured our app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div>Let's Goo 🚀🚀</div>;
+  return (
+    <>
+      <main>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <div className="app">
+          <Hero />
+          <Demo />
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default App;
